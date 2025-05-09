6158dc88a7756a6ed786caba06ab052278020d4c
llon24 <165463900+llon24@users.noreply.github.com>
2025-05-09 15:14:49 +0900

    세 번째 커밋밋

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,23 @@ function App() {
   };
   return (
     <>
-      <div className="relative flex flex-col w-full py-4 md:py-10 mx-auto space-y-4 md:space-y-10 bg-gray-900 text-lg/8 md:text-xl/9 md:w-3xl">
-        <DataStart />
-        <DataMonth />
-        <DataEnd />
-        {more && <DataEtc />}
+      <div className="mx-1 md:mx-4 break-keep">
+        <div
+          className={`relative flex flex-col w-full py-4 md:py-10 mx-auto space-y-4
+                      md:space-y-10 bg-gray-900 text-lg/8 md:text-2xl/15 md:max-w-7xl
+                      `}
+        >
+          <DataStart />
+          <DataMonth />
+          <DataEnd />
+          {more && <DataEtc />}
+        </div>
       </div>
       {showBtn && (
         <div className="relative">
-          <div className="fixed bottom-4 right-4 md:bottom-4 md:right-[max(calc(15px+((100%-768px)/2)),15px)] md:px-5">
+          <div className="fixed bottom-4 right-4 md:bottom-4 md:right-4 md:px-5">
             <button
-              className={`flex items-center justify-center w-12 h-12 text-2xl text-white 
+              className={`flex items-center justify-center w-12 h-12 md:w-16 md:h-16 text-4xl text-white 
                         bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
                         focus:ring-2 focus:outline-none focus:ring-cyan-300 rounded-full`}
               onClick={onClickMore}
